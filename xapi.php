f41df619c7a80a2510106d87e1f6178502863c9d
phish108 <cpglahn@gmail.com>
2013-08-27 21:12:21 +0200

    added loading for the auto loading class handler

--- xapi.php
+++ xapi.php
@@ -1,5 +1,9 @@
 <?php
 
+// Include the auto loading hooks for RESTling and our own classes
+include('include/RESTling/contrib/Restling.auto.php');
+include('include/PowerTLA.auto.php');
+
 require_once("include/ilServiceInit.php");  
 require_once("include/class.XAPIService.php");
 
