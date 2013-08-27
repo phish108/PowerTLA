fc3bc37bca6fc00ed7bda62c1b52e515539eca4a
phish108 <cpglahn@gmail.com>
2013-08-27 21:16:11 +0200

    Added class loader functions

--- oauth.php
+++ oauth.php
@@ -1,5 +1,9 @@
 <?php
 
+// Include the auto loading hooks for RESTling and our own classes
+include('include/RESTling/contrib/Restling.auto.php');
+include('include/PowerTLA.auto.php');
+
 require_once("include/ilServiceInit.php");     // the LMS functions
 require_once("include/class.AuthService.php"); // The service logic
 
