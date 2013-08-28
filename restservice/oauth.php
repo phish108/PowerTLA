11301675958e3b9cdd221ff73ee133dc02f9c75a
phish108 <cpglahn@gmail.com>
2013-08-28 14:29:20 +0200

    Corrected the include paths and added a few comments

--- restservice/oauth.php
+++ restservice/oauth.php
@@ -2,14 +2,15 @@
 
 // Include the auto loading hooks for RESTling and our own classes
 include('include/RESTling/contrib/Restling.auto.php');
-include('include/PowerTLA.auto.php');
+include('include/PowerTLA/PowerTLA.auto.php');
 
 require_once("include/ilServiceInit.php");     // the LMS functions
 require_once("include/class.AuthService.php"); // The service logic
 
 $plugins = array("oauth" => array("UIComponent", "uiroa", "OAuthREST"));
 
-$VLEAPI = new ilServiceInit($plugins); 
+// TODO: wrapper that decides which LMS initialization has to be used. 
+$VLEAPI = new IliasServiceInit($plugins); 
 $service = new AuthService($VLEAPI);
 
 $service->run();
--- restservice/xapi.php
+++ restservice/xapi.php
@@ -2,17 +2,19 @@
 
 // Include the auto loading hooks for RESTling and our own classes
 include('include/RESTling/contrib/Restling.auto.php');
-include('include/PowerTLA.auto.php');
+include('include/PowerTLA/PowerTLA.auto.php');
 
 require_once("include/ilServiceInit.php");  
 require_once("include/class.XAPIService.php");
 
 
-// TODO CHECK THE PLUGIN STATEMENTS
+// TODO: CHECK THE PLUGIN STATEMENTS
 $plugins = array("oauth" => array("UIComponent", "uiroa", "OAuthREST"),
                  "xapi"  => array("UIComponent", "uixapi", "XAPIREST"));
 
-$VLEAPI  = new ilServiceInit($plugins); 
+                 
+// TODO: wrapper that decides which LMS initialization has to be used. 
+$VLEAPI  = new IliasServiceInit($plugins); 
 $service = new XAPIService($VLEAPI);
 
 $service->run();
