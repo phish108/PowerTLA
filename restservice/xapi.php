76fb373a96c1411d5913beff2a15e0aea39989ea
Christian Glahn <cpglahn@gmail.com>
2014-09-03 15:04:35 +0200

    temporarily isolate the XAPI service

--- restservice/xapi.php
+++ restservice/xapi.php
@@ -1,23 +1,22 @@
 <?php
 
-set_include_path(".." . PATH_SEPARATOR . get_include_path());
+set_include_path(".." . PATH_SEPARATOR . "../include/PowerTLA/". PATH_SEPARATOR . get_include_path());
 
 // Include the auto loading hooks for RESTling and our own classes
-include('include/RESTling/contrib/Restling.auto.php');
-include('include/PowerTLA/PowerTLA.auto.php');
+include('../include/RESTling/contrib/Restling.auto.php');
+include('../include/PowerTLA/PowerTLA.auto.php');
 
-require_once('include/PowerTLA/Ilias/IliasHandler.class.php'); 
-require_once("include/class.XAPIService.php");
-
+// require_once('include/PowerTLA/Ilias/IliasHandler.class.php');
 
 // TODO: CHECK THE PLUGIN STATEMENTS
 $plugins = array("oauth" => array("UIComponent", "uiroa", "OAuthREST"),
                  "xapi"  => array("UIComponent", "uixapi", "XAPIREST"));
 
-                 
-// TODO: wrapper that decides which LMS initialization has to be used. 
-$VLEAPI  = new IliasHandler($plugins); 
-$service = new XAPIService($VLEAPI);
+
+// TODO: wrapper that decides which LMS initialization has to be used.
+//$VLEAPI  = new IliasHandler($plugins);
+
+$service = new XAPIService();
 
 $service->run();
 
