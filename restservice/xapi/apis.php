ea0a3602a4709c034afc048b6d67103651edf1a8
Christian Glahn <cpglahn@gmail.com>
2015-07-31 19:41:07 +0200

    LRS service root

---
+++ restservice/xapi/apis.php
@@ -0,0 +1,57 @@
+<?php
+
+$localpath = "restservice";
+
+if (!isset($pathprefix))
+{
+    $pathprefix = "";
+}
+else {
+    $pathprefix .= "/". $localpath;
+}
+
+if (!isset($service))
+{
+    // find ilias instance to load the metadata
+    $scwd = getcwd();
+    $cwd = explode('/', $scwd);
+    $ipath = "/include";
+    $xpath = array();
+
+    if (!file_exists("include/findVLE.php"))
+    {
+        while ($p = array_pop($cwd))
+        {
+            array_unshift($xpath, $p);
+            if (file_exists(implode('/', $cwd) . $ipath . "/findVLE.php"))
+            {
+                set_include_path(implode('/', $cwd). $ipath . PATH_SEPARATOR .
+                                 implode('/', $cwd). $ipath . "/PowerTLA". PATH_SEPARATOR .
+                                 get_include_path());
+                break;
+            }
+        }
+    }
+    else
+    {
+        // array_unshift($xpath, $localpath);
+        // because THIS script might run via our root rsd.php
+        set_include_path(implode('/', $cwd). $ipath . PATH_SEPARATOR .
+                         implode('/', $cwd). $ipath . "/PowerTLA". PATH_SEPARATOR .
+                         get_include_path());
+    }
+
+    require_once("findVLE.php");
+
+    $service = getVLEInstanceInformation(implode('/', $xpath));
+}
+
+include_once("learningcards/apis.php");
+include_once("content/apis.php");
+include_once("identity/apis.php");
+
+$ap = explode("/", $pathprefix);
+array_pop($ap);
+$pathprefix = implode("/", $ap);
+
+?>
---
+++ restservice/xapi/rsd.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once("apis.php");
+
+header('content-type: application/json');
+echo(json_encode($service));
+
+?>
