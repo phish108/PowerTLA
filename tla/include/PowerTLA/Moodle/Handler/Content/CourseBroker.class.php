6bda306946ab92c35b7a4987132ee432a2d3c269
Christian Glahn <cpglahn@gmail.com>
2016-07-15 09:47:46 +0200

    moodle course broker instance

--- tla/include/PowerTLA/Moodle/Handler/Content/CourseBroker.class.php
+++ tla/include/PowerTLA/Moodle/Handler/Content/CourseBroker.class.php
@@ -3,17 +3,56 @@ namespace PowerTLA\Moodle\Handler\Content;
 
 use PowerTLA\Handler\BaseHandler;
 
+include_once($CFG->libdir . '/moodlelib.php');
+include_once($CFG->dirroot . '/course/lib.php');
+include_once($CFG->libdir . '/coursecatlib.php');
+
 class CourseBroker extends BaseHandler
 {
 
     public function getUserCourse($courseId)
     {
         global $DB;
+
+        // return course details
+
+        return null;
     }
 
     public function getCourseList()
     {
-        global $DB;
+        $fieldMap = [
+            "id" => "course_id",
+            "short_name" => "shortname",
+            "display_name" => "fullname"
+        ];
+
+        $retval = [];
+
+        $sortorder = 'visible DESC, sortorder ASC';
+
+        if ($courses = enrol_get_my_courses(NULL, $sortorder)) {
+            foreach ($courses as $course) {
+                if (!$course->visible) {
+                    next;
+                }
+
+                $c = [];
+
+                foreach ($fieldMap as $k => $v) {
+                    if (property_exists($course, $k)) {
+                        $c[$v] = $course->$k;
+                    }
+                }
+
+                // ignore course files/images
+                // FIXME: add references to icons and images.
+
+                $retval[] = $c;
+            }
+        }
+
+        return $retval;
     }
 }
 
