072aa3c4b327f0a407c73b3386a9280c57c7de0e
Christian Glahn <cpglahn@gmail.com>
2017-01-02 21:56:21 +0100

    create anchors for the input handlers in the Security Model

--- src/Security/Ilias.php
+++ src/Security/Ilias.php
@@ -3,11 +3,11 @@
 namespace PowerTLA\Security;
 
 class Ilias implements \RESTling\Interfaces\Security\Model {
-    public function validateKey($key, $in) {
+    public function validateKey($key, $input) {
         throw new \RESTling\Exception\Security\SharedKeyValidationUnsupported();
     }
 
-    public function validateToken($token) {
+    public function validateToken($token, $input) {
         throw new \RESTling\Exception\Security\TokenValidationUnsupported();
     }
 }
--- src/Security/Moodle.php
+++ src/Security/Moodle.php
@@ -3,12 +3,15 @@
 namespace PowerTLA\Security;
 
 class Moodle implements \RESTling\Interfaces\Security\Model {
-    public function validateKey($key, $in) {
+
+    public function validateKey($key, $input) {
         throw new \RESTling\Exception\Security\SharedKeyValidationUnsupported();
+        // this function is called for all normal settings
     }
 
-    public function validateToken($token) {
+    public function validateToken($token, $input) {
         throw new \RESTling\Exception\Security\TokenValidationUnsupported();
+        // this function is called in a Authorization header is present.
     }
 }
 
