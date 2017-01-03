f078c78b07a6874e67632aa2ec71e2fa728fa23d
Christian Glahn <cpglahn@gmail.com>
2017-01-03 18:49:20 +0100

    more exceptions

---
+++ src/Exception/InvalidActorIdentifier.php
@@ -0,0 +1,6 @@
+<?php
+namespace PowerTLA\Exception;
+
+class InvalidActorIdentifier extends \RESTling\Exception {
+}
+?>
---
+++ src/Exception/InvalidWebfingerResourceUri.php
@@ -0,0 +1,6 @@
+<?php
+namespace PowerTLA\Exception;
+
+class InvalidWebfingerResourceUri extends \RESTling\Exception {
+}
+?>
