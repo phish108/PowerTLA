0e5bd7b7a759e915548a3d9e9ba22e371b231768
Christian Glahn <cpglahn@gmail.com>
2017-01-19 23:48:50 +0100

    add jet handling to the security model

--- src/Security/Moodle.php
+++ src/Security/Moodle.php
@@ -4,7 +4,41 @@ namespace PowerTLA\Security;
 
 class Moodle extends \PowerTLA\Security {
 
-    public function validateKey($key, $source) {
+    public function getSharedKey($kid, $jku) {
+        global $DB;
+
+        if (empty($kid) && empty($jku)) {
+            throw new \RESTling\Exception\Forbidden();
+        }
+        $attr = [];
+        if (empty($kid)) {
+            $attr["kid"] = null;
+        }
+        else {
+            $attr["kid"] = $kid;
+        }
+        if (empty($jku)) {
+            $attr["jku"] = null;
+        }
+        else {
+            $attr["jku"] = $jku;
+        }
+
+        $kinfo = $DB->get_records("auth_oauth_keys", $attr);
+        if (count($kinfo) == 1) {
+            $ki = $kinfo[0];
+        }
+        else {
+            // not unique , reject
+            throw new \RESTling\Exception\Forbidden();
+        }
+        return $ki->crypt_key;
+    }
+
+    /**
+ 	 * handle moodle's external tokens as cookies or query keys
+	 */
+	public function validateKey($key, $source) {
         $source = ucfirst(strtolower($source));
         $fname = "validateKey$source";
 
@@ -15,7 +49,14 @@ class Moodle extends \PowerTLA\Security {
         $this->$fname($key);
     }
 
-    public function translateTokenName($tokenname, $source) {
+    /**
+ 	 * Helper Function to transpose session cookies
+ 	 *
+ 	 * @param string $tokenname
+     * @param string $source - where to find the foken
+ 	 * @return void
+	 */
+	public function translateTokenName($tokenname, $source) {
         if (strtolower($source) === "cookie" &&
             $tokenname === strtolower('session')) {
             return 'MoodleSession'; // this is necessary, so the cookie validation succeeds
@@ -23,7 +64,13 @@ class Moodle extends \PowerTLA\Security {
         return $tokenname;
     }
 
-    public function validateToken($token, $source) {
+    /**
+ 	 * handle moodle external tokens as flat Bearer Tokens
+ 	 *
+ 	 * @param type
+ 	 * @return void
+	 */
+	public function validateToken($token) {
         global $DB;
 
         throw new \RESTling\Exception\Security\TokenValidationUnsupported();
@@ -37,8 +84,9 @@ class Moodle extends \PowerTLA\Security {
                 // limit moodle what it can do
                 $DB->delete_records('external_tokens',     ['token' => $token]);
 
-                // we MUST delete also the oauth token
-                $DB->delete_records('pwrtla_oauth_tokens', ["access_token" => $token]);
+                // we MUST NOT delete the oauth token
+                // $DB->delete_records('auth_oauth_tokens', ["access_token" => $token]);
+                // instead we wait for the refresh token
                 throw new \RESTling\Exception\Unauthorized();
             }
 
@@ -50,12 +98,12 @@ class Moodle extends \PowerTLA\Security {
         }
         else {
             // check if we have the token in our internal records (for auth agents)
-            $dbToken = $DB->get_record('pwrtla_oauth_tokens', ["access_token" => $token]);
+            $dbToken = $DB->get_record('auth_oauth_tokens', ["access_token" => $token]);
             if (!$dbToken) {
                 throw new \RESTling\Exception\Unauthorized();
             }
             if ($ts > $dbToken->expires) {
-                $DB->delete_records('pwrtla_oauth_tokens', ["access_token" => $token]);
+                $DB->delete_records('auth_oauth_tokens', ["access_token" => $token]);
                 throw new \RESTling\Exception\Unauthorized();
             }
         }
